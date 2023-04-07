ed3a6017ad3a720d3233a606c0f0f9057a26b1bb
mrdoan <josdoankiem@gmail.com>
2023-04-07 17:58:50 +0700

    complete group job

--- asyncjob/group.go
+++ asyncjob/group.go
@@ -6,34 +6,74 @@ import (
 	"sync"
 )
 
-type Group struct {
+type group struct {
 	Jobs       []job
 	isCurrency bool
+	wg         *sync.WaitGroup
 }
 
-var wg *sync.WaitGroup
-
-func NewGroup(isConcurency bool, jobs ...job) *Group {
-	return &Group{
+func NewGroup(isConcurency bool, jobs ...job) *group {
+	return &group{
 		isCurrency: isConcurency,
 		Jobs:       jobs,
+		wg:         new(sync.WaitGroup),
 	}
 }
-func (g Group) Run(ctx context.Context) error {
+func (g *group) Run(ctx context.Context) error {
+	g.wg.Add(len(g.Jobs))
+
+	errChan := make(chan error, len(g.Jobs))
+
+	//run job
+	for i, _ := range g.Jobs {
+		if g.isCurrency {
+			//run job concurrency
+			go func(aj *job) {
+				errChan <- g.runJob(ctx, aj)
+				g.wg.Done()
+			}(&g.Jobs[i])
+			continue
+		}
+
+		//run sequence
+		job := g.Jobs[i]
+		errChan <- g.runJob(ctx, &job)
+		g.wg.Done()
+	}
 
 	var err error
-	for _, job := range g.Jobs {
-		jerr := job.Execute(ctx)
-		if jerr != nil {
-			log.Printf("job err: %v", jerr)
-			err = jerr
+
+	for i := 0; i < len(g.Jobs); i++ {
+		if v := <-errChan; v != nil {
+			err = v
 		}
 	}
 
+	g.wg.Wait()
 	return err
 }
 
-func (g *Group) AddJob(jobs ...job) {
+func (g *group) runJob(ctx context.Context, job *job) error {
+
+	var e error
+	if e = job.Execute(ctx); e != nil {
+		log.Printf("Job error when execute: %v", e)
+		for {
+			if e = job.Retry(ctx); e == nil {
+				break
+			}
+
+			if job.State() == StateRetryFailed {
+				break
+			}
+		}
+		return e
+	}
+
+	return nil
+}
+
+func (g *group) AddJob(jobs ...job) {
 	for _, j := range jobs {
 		g.Jobs = append(g.Jobs, j)
 	}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
 	"time"
 
@@ -11,53 +10,35 @@ import (
 
 func main() {
 	ctx := context.Background()
-	j0 := asyncjob.NewJob(func(ctx context.Context) error {
-		log.Println("Testing job 0")
-		return errors.New("error something")
+
+	j1 := asyncjob.NewJob(func(ctx context.Context) error {
+		log.Println("Test job A")
+		log.Println("job A sleeping 1s")
+		time.Sleep(time.Second)
+		return nil
+		//return errors.New("A: error something")
 	})
-	j0.SetRetryTime([]time.Duration{time.Second * 2, time.Second * 5})
+	j1.SetRetryTime([]time.Duration{time.Second * 2, time.Second * 5})
 
-	if e := j0.Execute(ctx); e != nil {
-		log.Printf("Job 0 error when execute: %v", e)
-		for {
-			if err := j0.Retry(ctx); err == nil {
-				break
-			}
+	j2 := asyncjob.NewJob(func(ctx context.Context) error {
+		log.Println("Test job B")
 
-			if j0.State() == asyncjob.StateRetryFailed {
-				break
-			}
-			log.Printf("Job 0 error when retry: %v", e)
-		}
-	}
+		time.Sleep(3 * time.Second)
+		log.Println("job B sleeping 3s")
+		return nil
+	})
+	j2.SetRetryTime([]time.Duration{time.Second * 3, time.Second * 3})
 
-	//j1 := asyncjob.NewJob(func(ctx context.Context) error {
-	//	log.Println("Test job A")
-	//	log.Println("job A sleeping 1s")
-	//	return errors.New("A: error something")
-	//})
-	//j1.SetRetryTime([]time.Duration{time.Second * 2, time.Second * 5})
-	//
-	//j2 := asyncjob.NewJob(func(ctx context.Context) error {
-	//	log.Println("Test job B")
-	//
-	//	time.Sleep(3 * time.Second)
-	//	log.Println("job B sleeping 3s")
-	//	return nil
-	//})
-	//j2.SetRetryTime([]time.Duration{time.Second * 3, time.Second * 3})
-	//
-	//j3 := asyncjob.NewJob(func(ctx context.Context) error {
-	//	log.Println("Test job C")
-	//
-	//	time.Sleep(3 * time.Second)
-	//	log.Println("job C sleeping 5s")
-	//	return errors.New("C: error something")
-	//})
-	//j3.SetRetryTime([]time.Duration{time.Second, time.Second * 3})
-	//
-	//group := asyncjob.NewGroup(true, j1, j2, j3)
-	//group.Run(ctx)
+	j3 := asyncjob.NewJob(func(ctx context.Context) error {
+		log.Println("Test job C")
 
-	time.Sleep(10 * time.Second)
+		time.Sleep(1 * time.Second)
+		log.Println("job C sleeping 1s")
+		return nil
+	})
+	j3.SetRetryTime([]time.Duration{time.Second, time.Second * 3})
+
+	group := asyncjob.NewGroup(true, j1, j2, j3)
+	group.Run(ctx)
+
 }
